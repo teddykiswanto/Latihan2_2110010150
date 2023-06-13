fcbb90d21c1fb220f9e86acbf40662a3f95ad84d
teddykiswanto <teddykiswanto17@gmail.com>
2023-06-13 09:00:28 +0800

    Membuat class Mahasiswa dan Mahasiswa Beraksi

---
+++ src/universitas/Mahasiswa.java
@@ -0,0 +1,28 @@
+package universitas;
+
+
+public class Mahasiswa {
+    String npm;
+    String nama;
+    String prodi;
+    
+        public String getNama(){
+       return nama;  
+   }
+    public String getNpm(){
+       return npm;  
+   }
+    public String getProdi(){
+       return prodi;  
+   }
+    
+    public void setnama (String nama){
+       nama = nama;
+   }
+    
+    public void setnpm (String npm){
+       npm = npm;
+   }
+    
+
+}
---
+++ src/universitas/MahasiswaBeraksi.java
@@ -0,0 +1,12 @@
+package universitas;
+
+
+public class MahasiswaBeraksi {
+    public static void main(String[] args) {
+        Mahasiswa Teddy = new Mahasiswa ("2110010150","Teddy Kiswanto","TI");
+                
+                System.out.printl ("npm Mahasiswa : "+teddy.getNama());
+                System.out.printl ("nama Mahasiswa : "+teddy.getNpm());
+                System.out.printl ("prodi Mahasiswa : "+teddy.getProdi());
+    }
+}
